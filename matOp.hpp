7351c9df8e6e6b43ed02b031ae30e957934a0217
Marcos Rodríguez <marcos@unizar.es>
2018-03-08 13:59:00 +0100

    OPERATION classes in macros

--- matOp.hpp
+++ matOp.hpp
@@ -1,6 +1,29 @@
 #ifndef __MATRIX_OP_HPP__
 #define __MATRIX_OP_HPP__
 
+
+// MACRO FOR UNARY MATRIX OPERATION
+#define UNARY_MAT_OPERATION(className) \
+  template <class T>\
+  class className : public UnaryMatOperation_<T> {\
+  public:\
+    className(const VirtualMat_<T> &op)\
+        : UnaryMatOperation_<T>(op) {}\
+    void eval () const;\
+  };
+
+// MACRO FOR BINARY MATRIX OPERATION
+#define BINARY_MAT_OPERATION(className) \
+  template <class T>\
+  class className : public BinaryMatOperation_<T> {\
+  public:\
+    className(const VirtualMat_<T> &op1, const VirtualMat_<T> &op2)\
+        : BinaryMatOperation_<T>(op1, op2) {}\
+    void eval () const;\
+  };
+
+
+
 #include "vmat.hpp"
 
 template <class T>
@@ -29,32 +52,20 @@ public:
 };
 
 // ADDITION
+BINARY_MAT_OPERATION(MatAdd_)
 template <class T>
-class MatAdd_ : public BinaryMatOperation_<T> {
-public:
-
-  MatAdd_(const VirtualMat_<T> &op1, const VirtualMat_<T> &op2)
-      : BinaryMatOperation_<T>(op1, op2) {
-    if(op1.rows != op2.rows || op1.cols != op2.cols) {
-      std::cerr << "addition dimmensions missmatch" << std::endl;
-      exit(1);
-    }
+void MatAdd_<T>::eval () const {
+  if(this->data != NULL) {
+    std::cerr << "reevaluation" << std::endl;
+    exit(1);
   }
-
-  void eval () const {
-    if(this->data != NULL) {
-      std::cerr << "reevaluation" << std::endl;
-      exit(1);
-    }
-    this->op1->eval(); this->op2->eval();
-    this->data = new int[this->rows*this->cols];
-    for(int i=0; i<this->rows; ++i) for(int j=0; j<this->cols; ++j)
-      this->data[this->cols*i+j] =
-          this->op1->data[this->op1->cols*i+j]
-          + this->op2->data[this->op2->cols*i+j];
-  }
-};
-
+  this->op1->eval(); this->op2->eval();
+  this->data = new int[this->rows*this->cols];
+  for(int i=0; i<this->rows; ++i) for(int j=0; j<this->cols; ++j)
+  this->data[this->cols*i+j] =
+  this->op1->data[this->op1->cols*i+j]
+  + this->op2->data[this->op2->cols*i+j];
+}
 template <class T>
 MatAdd_<T> operator+(const VirtualMat_<T> &op1, const VirtualMat_<T> &op2) {
   MatAdd_<T> rop(op1, op2);
@@ -63,55 +74,39 @@ MatAdd_<T> operator+(const VirtualMat_<T> &op1, const VirtualMat_<T> &op2) {
 
 
 // SUBSTRACTION
+BINARY_MAT_OPERATION(MatSub_)
 template <class T>
-class MatSub_ : public BinaryMatOperation_<T> {
-public:
-
-  MatSub_(const VirtualMat_<T> &op1, const VirtualMat_<T> &op2)
-      : BinaryMatOperation_<T>(op1, op2) {
-    if(op1.rows != op2.rows || op1.cols != op2.cols) {
-      std::cerr << "addition dimmensions missmatch" << std::endl;
-      exit(1);
-    }
+void MatSub_<T>::eval () const {
+  if(this->data != NULL) {
+    std::cerr << "reevaluation" << std::endl;
+    exit(1);
   }
-
-  void eval () const {
-    if(this->data != NULL) {
-      std::cerr << "reevaluation" << std::endl;
-      exit(1);
-    }
-    this->op1->eval(); this->op2->eval();
-    this->data = new int[this->rows*this->cols];
-    for(int i=0; i<this->rows; ++i) for(int j=0; j<this->cols; ++j)
-      this->data[this->cols*i+j] =
-          this->op1->data[this->op1->cols*i+j]
-          - this->op2->data[this->op2->cols*i+j];
-  }
-};
-
+  this->op1->eval(); this->op2->eval();
+  this->data = new int[this->rows*this->cols];
+  for(int i=0; i<this->rows; ++i) for(int j=0; j<this->cols; ++j)
+    this->data[this->cols*i+j] =
+        this->op1->data[this->op1->cols*i+j]
+        - this->op2->data[this->op2->cols*i+j];
+}
 template <class T>
 MatSub_<T> operator-(const VirtualMat_<T> &op1, const VirtualMat_<T> &op2) {
   MatSub_<T> rop(op1, op2);
   return rop;
 }
 
+// MATRIX MINUS UNARY OPERATOR
+UNARY_MAT_OPERATION(MatNeg_)
 template <class T>
-class MatNeg_ : public UnaryMatOperation_<T> {
-public:
-  MatNeg_(const VirtualMat_<T> &op)
-      : UnaryMatOperation_<T>(op) {}
-  void eval () const {
-    if(this->data != NULL) {
-      std::cerr << "reevaluation" << std::endl;
-      exit(1);
-    }
-    this->op->eval();
-    this->data = new T[this->rows*this->cols];
-    for(int i=0; i<this->rows; ++i) for(int j=0; j<this->cols; ++j)
-      this->data[this->cols*i+j] = -this->op->data[this->op->cols*i+j];
+void MatNeg_<T>::eval () const {
+  if(this->data != NULL) {
+    std::cerr << "reevaluation" << std::endl;
+    exit(1);
   }
-};
-
+  this->op->eval();
+  this->data = new T[this->rows*this->cols];
+  for(int i=0; i<this->rows; ++i) for(int j=0; j<this->cols; ++j)
+    this->data[this->cols*i+j] = -this->op->data[this->op->cols*i+j];
+}
 template <class T>
 MatNeg_<T> operator-(const VirtualMat_<T> &op) {
   MatNeg_<T> rop(op);
